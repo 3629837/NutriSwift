047cce56c696da799becaa2dcc0ff71f55a03a7d
Hayden <farrierhayden@gmail.com>
2018-01-19 18:25:02 +1100

    Added functionality to DiaryTableViewController. Added folders to project for easier readability

---
+++ NutriSwift/NutriSwift/Controllers/DiaryTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DiaryTableViewController.swift
+//  NutriSwift
+//
+//  Created by Hayden Farrier on 1/19/18.
+//  Copyright © 2018 Hayden Farrier. All rights reserved.
+//
+
+import UIKit
+
+class DiaryTableViewController: UITableViewController {
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.tableView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section Name Placeholder"
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        let label1 = cell.viewWithTag(1001) as! UILabel
+        return cell
+    }
+    
+}
