159421b0b41fdb8873fdacd132e1f8060fbf8ebf
rmit-s3503519-asli-yoruk <s3503519@student.rmit.edu.au>
2018-01-23 12:28:43 +1100

    Food Info Page added

---
+++ NutriSwift/NutriSwift/New Group/FoodEntryController.swift
@@ -0,0 +1,29 @@
+//
+//  FoodEntryController.swift
+//  NutriSwift
+//
+//  Created by Aslı Yörük on 23/1/18.
+//  Copyright © 2018 Hayden Farrier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FoodEntryController: UIViewController {
+    
+    @IBAction func foodAdd(_ sender: Any) {
+    }
+    @IBAction func foodEntered(_ sender: UITextField) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
